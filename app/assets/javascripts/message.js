e4ec5fd156ed67420e24eec12d382c3126266b4b
kazu8901 <ka_8901@docomo.ne.jp>
2020-06-25 21:26:22 +0900

    Create message.js

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,61 @@
+$(function() {
+     
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+       `<div class="name">
+          ${message.user_name}
+        </div>
+        <div class="name__date">
+          ${message.created_at}
+        </div>
+        <div class="message">
+          <p>
+            ${message.content}
+          </p>
+        </div>
+        <img src=${message.image} >`
+      return html;
+    } else {
+      var html =
+       `<div class="name">
+          ${message.user_name}
+        </div>
+        <div class="name__date">
+          ${message.created_at}
+        </div>
+        <div class="message">
+          <p>
+            ${message.content}
+          </p>
+        </div>`
+      return html;
+    };
+  }
+
+  $('#new_message').on('submit', function(e){
+    console.log("new")
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      $('.btn').prop('disabled', false);
+      $('form')[0].reset();
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
+  })
+
+});
